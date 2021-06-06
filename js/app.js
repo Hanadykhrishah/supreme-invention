ef56a415594d9de7d3f146d6bb6f65a2b5340b3f
Hanady khrishah <hanady.awad94@yahoo.com>
2021-06-06 16:11:06 +0300

    structure with html,style with css and js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let userName = prompt ('please Enter Your Name!')
+alert ('welcome to my webpage ' + userName )
+let favPit = prompt ('What is Your Fav animal!!')
+alert ('What about panda!!!')
+let panda = prompt ('please choose panda , We will have fun together')
+alert ('You are so Kind !')
+let user = prompt ('Have you seen me before ?, My picture is below!')
+
+
+
+
+
